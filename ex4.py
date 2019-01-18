05439c67be4bf014bdd7882df61881d37785eb70
elad.bendavid <elad.bendavid1@cs.huji.ac.il>
2019-01-18 15:14:42 +0200

    find_consecutive_propn implemented

--- ex4.py
+++ ex4.py
@@ -20,17 +20,23 @@ def main():
     print(consecutive_propn)
 
 
-def find_consecutive_propn(analyzed_page: tokens.doc.Doc) ->set:
-    proper_nouns = set()
-    last_pos = analyzed_page[0].pos_
-    last_word = analyzed_page[0].text
+def find_consecutive_propn(analyzed_page: tokens.doc.Doc) ->list:
+    current_sequence = list()
+    sequences = list()
+    is_consecutive_sequence = False
     for word in analyzed_page:
         current_pos = word.pos_
-        current_word = word.text
-        if last_pos == PROPN and current_pos == PROPN:
-            proper_nouns.add(last_word)
-            proper_nouns.add(current_word)
-    return proper_nouns
+        if current_pos == PROPN:
+            if not is_consecutive_sequence:
+                current_sequence = list()
+            current_sequence.append(word)
+            is_consecutive_sequence = True
+        else:
+            if is_consecutive_sequence:
+                sequences.append(current_sequence)
+            is_consecutive_sequence = False
+    return sequences
+
 
 def find_consecutive_nouns(analyzed_page: tokens.doc.Doc) ->set:
     proper_nouns_paires = set()
