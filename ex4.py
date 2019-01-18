12961966eb792e8261fc133f7d247ea0facfe7a5
shaked571 <shakedgreenfeld@gmail.com>
2019-01-18 14:29:05 +0200

    with good logger

--- ex4.py
+++ ex4.py
@@ -1,16 +1,20 @@
 import logging
+import wikipedia
 import spacy
-import numpy as _nx
+import datetime
+time = datetime.datetime.now()
 
-logging.basicConfig(filename='myapp.log', level=logging.INFO)
-log = logging.getLogger('myLogger')
+logging.basicConfig(filename=f"logs\ex4_{time.strftime('%d_%H_%M_%S')}.log", level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger('myLogger')
 
 
 def main():
-    nlp = spacy.load('en')
-    doc = nlp(u"This is a sentence.")
-    log.info([(w.text, w.pos_) for w in doc])
-    _nx.AxisError(1)
+    nlp_model = spacy.load('en')
+    page = wikipedia.page('Brad Pitt').content
+    analyzed_page = nlp_model(page)
+    doc = nlp_model(u"AMZN achieved a very big profit in Q3 2017, we believe it would increase in the upcoming years")
+    logger.info([(w.text, w.pos_) for w in doc])
 
 
 
