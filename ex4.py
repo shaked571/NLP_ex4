1aa7401ea25d9991c5dfc262bf46401444ee564e
shaked571 <shakedgreenfeld@gmail.com>
2019-01-18 13:45:15 +0200

    with test ready

--- ex4.py
+++ ex4.py
@@ -1,25 +1,17 @@
 import logging
 import spacy
+import numpy as _nx
 
 logging.basicConfig(filename='myapp.log', level=logging.INFO)
 log = logging.getLogger('myLogger')
 
 
-class Global:
-    SourceIPAddress = ''
-
-
-class IpFilter(logging.Filter):
-    def filter(self, rec):
-        return not Global.SourceIPAddress == '127.0.0.1'
-
-
 def main():
-    spacy.load('en')
-    log.addFilter(IpFilter())
-    log.info("work")
-    Global.SourceIPAddress = '127.0.0.1'
-    log.info("Dont log")
+    nlp = spacy.load('en')
+    doc = nlp(u"This is a sentence.")
+    log.info([(w.text, w.pos_) for w in doc])
+    _nx.AxisError(1)
+
 
 
 if __name__ == '__main__':
