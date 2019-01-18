97decb0ddc7842f78d6ffdc966d59baf9c312ae1
shaked571 <shakedgreenfeld@gmail.com>
2019-01-18 16:54:52 +0200

    find consecutive pairs of proper nouns

--- ex4.py
+++ ex4.py
@@ -3,7 +3,10 @@ import wikipedia
 import spacy
 import datetime
 from spacy import tokens
+
+PUNCT = "PUNCT"
 PROPN = "PROPN"
+VERB = "VERB"
 
 time = datetime.datetime.now()
 logging.basicConfig(filename=f"logs\\ex4_{time.strftime('%d_%H_%M_%S')}.log", level=logging.INFO,
@@ -15,9 +18,24 @@ def main():
     nlp_model = spacy.load('en')
     page = wikipedia.page('Brad Pitt').content
     analyzed_page = nlp_model(page)
-    logger.info(f"the type is: {type(analyzed_page)}")
-    consecutive_propn = find_consecutive_propn(analyzed_page)
-    print(consecutive_propn)
+    doc = nlp_model(u"AMZN is a great company, John told this to Jerry which later went to Rafael the king. ")
+    doc2 = nlp_model(u"The company Apple is better than Microsoft.")
+    doc3 = nlp_model(u"AMZN is the best! Apple is OK.")
+    doc4 = nlp_model(u"John! come here you.")
+    doc5 = nlp_model(u"John and Mike went to MIT university.")
+    logger.info(([(w.text, w.pos_) for w in doc]))
+    logger.info(find_consecutive_nouns_pairs(doc))
+    logger.info(([(w.text, w.pos_) for w in doc2]))
+    logger.info(find_consecutive_nouns_pairs(doc2))
+    logger.info(([(w.text, w.pos_) for w in doc3]))
+    logger.info(find_consecutive_nouns_pairs(doc3))
+    logger.info(([(w.text, w.pos_) for w in doc4]))
+    logger.info(find_consecutive_nouns_pairs(doc4))
+    logger.info(([(w.text, w.pos_) for w in doc5]))
+    logger.info(find_consecutive_nouns_pairs(doc5))
+    # logger.info(f"the type is: {type(analyzed_page)}")
+    # consecutive_propn = find_consecutive_propn(analyzed_page)
+    # print(consecutive_propn)
 
 
 def find_consecutive_propn(analyzed_page: tokens.doc.Doc) ->list:
@@ -38,9 +56,27 @@ def find_consecutive_propn(analyzed_page: tokens.doc.Doc) ->list:
     return sequences
 
 
-def find_consecutive_nouns(analyzed_page: tokens.doc.Doc) ->set:
-    proper_nouns_paires = set()
 
+def find_consecutive_nouns_pairs(analyzed_page: tokens.doc.Doc) -> list:
+    proper_nouns_pairs = list()
+    candidate_word = None
+    is_verb_exist = False
+    relation_verb = None
+    for word in analyzed_page:
+        current_pos = word.pos_
+        if current_pos == PROPN:
+            if candidate_word is not None and is_verb_exist:
+                proper_nouns_pairs.append((candidate_word, relation_verb,word))
+            candidate_word = word
+        elif current_pos == PUNCT:
+            candidate_word = None
+            is_verb_exist = False
+        elif current_pos == VERB:
+            relation_verb = word
+            is_verb_exist = True
+        else:
+            continue
+    return proper_nouns_pairs
 
 
 if __name__ == '__main__':
